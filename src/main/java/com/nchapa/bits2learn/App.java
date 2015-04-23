bcd849dd9e9e31d386afbc9c3f8cd9bebddbc1b9
bits2learn <bits2learn@users.noreply.github.com>
2015-04-22 23:05:47 -0700

    Updated App class with Servlet extension

--- src/main/java/com/nchapa/bits2learn/App.java
+++ src/main/java/com/nchapa/bits2learn/App.java
@@ -1,13 +1,31 @@
 package com.nchapa.bits2learn;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends HttpServlet
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+    throws ServletException, IOException {
+        
+        showHome(req, resp);
+        
+    }
+    
+    private void showHome(HttpServletRequest req, HttpServletResponse resp)
+    throws ServletException, IOException {
+        
+        resp.getWriter().print("Hello World!");
     }
 }
